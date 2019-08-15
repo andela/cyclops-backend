85c5838fca7f1ff8c19abe46be38b14f03fcdced
Imo-owo Nabuk <richienabuk@gmail.com>
2019-08-15 21:48:37 +0100

    Restore body-parser package

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the Cyclops Barefoot Nomad backend API',
