b36ce19e694dc6d80e5d9e95517a06d354ed1c30
Blessing Abobo <suspieabobo@yahoo.com>
2019-08-19 19:15:01 +0100

    ch(test): Setup unit tests
    - Install mocha chai and chai-http dependencies
    - Write unit test for GET route to "/"
    - Specify a test script in package.json
    
    [Finishes #167728008]

---
+++ tests/server.test.js
@@ -0,0 +1,32 @@
+import { describe, it } from 'mocha';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/index';
+
+chai.use(chaiHttp);
+
+describe('App', () => {
+  it('Should display a Welcome to the Cyclops Barefoot Nomad backend API', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.status).eql(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.eql('success');
+        expect(res.body.data).to.eql('Welcome to the Cyclops Barefoot Nomad backend API');
+        done();
+      });
+  });
+
+  it('Should display a This route is unavailable on this serve', (done) => {
+    chai.request(app)
+      .get('/hjhjh')
+      .end((err, res) => {
+        expect(res.status).eql(404);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.eql('error');
+        expect(res.body.error).to.eql('This route is unavailable on this server');
+        done();
+      });
+  });
+});
