08c42be765b3f53d1bff2be35338998e5e33d133
awadieudone <dieudonneawa7@gmail.com>
2019-08-23 16:59:35 +0100

    ch(jsonwebtoken):process json web token
    [Finishes #168052207]

---
+++ tests/tokenProcessor.test.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+import { describe, it } from 'mocha';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { createToken, verifyToken } from '../src/modules/tokenProcessor';
+
+chai.use(chaiHttp);
+
+describe('Process token', () => {
+  const email = 'somemail@mail.com';
+  const token = createToken({ email });
+  const verifiedToken = verifyToken(token);
+  describe('Test Create Token', () => {
+    it('Should return a token string', (done) => {
+      expect(token).to.be.a('string');
+      done();
+    });
+  });
+
+  describe('Test Verify Token', () => {
+    it('Should return an object', (done) => {
+      expect(verifiedToken).to.be.an('object');
+      done();
+    });
+  });
+});
