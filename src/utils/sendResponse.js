a4c4e5c4409c796e50e5199bb98f66fb5fee2f65
Imo-owo Nabuk <richienabuk@gmail.com>
2019-08-26 11:40:48 +0100

    ft(user): User can sign in with pre-existing Google or Facebook account

--- src/utils/sendResponse.js
+++ src/utils/sendResponse.js
@@ -1,8 +1,9 @@
-const sendErrorResponse = (res, code, errorMessage) => {
-  res.status(code).json({
-    status: 'error',
-    error: errorMessage,
-  });
-};
+export const sendErrorResponse = (res, code, errorMessage) => res.status(code).send({
+  status: 'error',
+  error: errorMessage,
+});
 
-export default sendErrorResponse;
+export const sendSuccessResponse = (res, code, data) => res.status(code).send({
+  status: 'success',
+  data
+});
