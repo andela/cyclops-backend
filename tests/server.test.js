ed684f2381704e07a78fb75a7adc7169a43dec8a
Imo-owo Nabuk <richienabuk@gmail.com>
2019-08-26 11:40:48 +0100

    ft(user): User can sign in with pre-existing Google or Facebook account

--- tests/server.test.js
+++ tests/server.test.js
@@ -18,9 +18,45 @@ describe('App', () => {
       });
   });
 
-  it('Should display a This route is unavailable on this serve', (done) => {
+  it('Should display a This route is unavailable on this serve for GET method', (done) => {
     chai.request(app)
       .get('/api/v1/hjhjh')
+      .end((err, res) => {
+        expect(res.status).eql(404);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.eql('error');
+        expect(res.body.error).to.eql('This route is unavailable on this server');
+        done();
+      });
+  });
+
+  it('Should display a This route is unavailable on this serve for POST method', (done) => {
+    chai.request(app)
+      .post('/api/v1/hjhjh')
+      .end((err, res) => {
+        expect(res.status).eql(404);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.eql('error');
+        expect(res.body.error).to.eql('This route is unavailable on this server');
+        done();
+      });
+  });
+
+  it('Should display a This route is unavailable on this serve for PATCH method', (done) => {
+    chai.request(app)
+      .patch('/api/v1/hjhjh')
+      .end((err, res) => {
+        expect(res.status).eql(404);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.eql('error');
+        expect(res.body.error).to.eql('This route is unavailable on this server');
+        done();
+      });
+  });
+
+  it('Should display a This route is unavailable on this serve for DELETE method', (done) => {
+    chai.request(app)
+      .delete('/api/v1/hjhjh')
       .end((err, res) => {
         expect(res.status).eql(404);
         expect(res.body).to.be.an('object');
