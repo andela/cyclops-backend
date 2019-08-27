ae22507ff440a203f57891d32dc3343cafa1b50c
Imo-owo Nabuk <richienabuk@gmail.com>
2019-08-27 21:10:04 +0100

    ft(socialOAuth): Fix hound errors

--- src/repositories/SocialOAuthRepository.js
+++ src/repositories/SocialOAuthRepository.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /**
  * @fileoverview Contains the Social OAuth Repository class, an interface for querying User table
  *
@@ -6,10 +7,9 @@
  * @requires models/User.js
  */
 
-import { sendSuccessResponse, sendErrorResponse } from '../utils/sendResponse';
 import Model from '../models';
 
- const { User } = Model;
+const { User } = Model;
 /**
  * User repository class
  *
@@ -22,35 +22,36 @@ export default class SocialOAuthRepository {
   constructor() {
     this.model = User;
   }
-   /**
+
+  /**
    * @description Returns the newly created user details
-   * 
+   *
    * @param {String} param0 social OAuth details
-   * 
+   *
    * @return {Object} returns new or existing user details
    */
   static async create({
-    social_id, 
-    name, 
-    image, 
-    email, 
+    social_id,
+    name,
+    image,
+    email,
     provider
   }) {
     const user = provider === 'facebook'
-        ? await User.findOne({ where: { facebook_id: social_id } })
-        : await User.findOne({ where: { google_id: social_id } });
-      if (user) return sendSuccessResponse(res, 200, user);
+      ? await User.findOne({ where: { facebook_id: social_id } })
+      : await User.findOne({ where: { google_id: social_id } });
+    if (user) return user;
 
-      const { dataValues } = await User.create({
-        name,
-        email,
-        is_verified: true,
-        image_url: image,
-        facebook_id: (provider === 'facebook' ? social_id : ''),
-        google_id: (provider === 'google' ? social_id : ''),
-        role: 'employee'
+    const { dataValues } = await User.create({
+      name,
+      email,
+      is_verified: true,
+      image_url: image,
+      facebook_id: (provider === 'facebook' ? social_id : ''),
+      google_id: (provider === 'google' ? social_id : ''),
+      role: 'employee'
       // eslint-disable-next-line arrow-parens
-      });
-      return dataValues;
+    });
+    return dataValues;
   }
 }
